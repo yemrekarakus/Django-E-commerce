48e2c263da0d89165bded753bc58f97ed40a8d48
yunusemrekarakus <yunusemreiletisim23@gmail.com>
2022-02-23 22:41:18 +0300

    static files conf. - index page added

--- page/views.py
+++ page/views.py
@@ -2,4 +2,4 @@ from multiprocessing import context
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'page/index.html', context={})
+    return render(request, 'home/index.html', context={})
