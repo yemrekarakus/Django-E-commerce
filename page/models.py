2fdb1b97e0b37c436561f76b7327d45e5a9fa209
yunusemrekarakus <yunusemreiletisim23@gmail.com>
2022-02-23 23:12:46 +0300

    Media conf. | Page model added

--- page/models.py
+++ page/models.py
@@ -1,3 +1,22 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+STATUS = [
+    ('draft', 'Taslak'),
+    ('published', 'Yayınlanmış'),
+    ('deleted', 'Silindi'),
+]
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=180)
+    # slug = 
+    content = models.TextField()
+    cover_image = models.ImageField(upload_to='page')
+    status = models.CharField(
+        default="draft",
+        choices=STATUS,
+        max_length=12,
+    )
+    createt_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
